3a7cedfadac86b9403771dbb83cbac49886f9404
TORIQ <tariqulislambd01@gmail.com>
2020-03-01 19:10:15 +0600

    code added

--- shopCart.js
+++ shopCart.js
@@ -1,30 +1,22 @@
 // Plus Minus Button Start
-const btnAdd = document.getElementById("plusBtn");
-const btnAdd2 = document.getElementById("plusBtn2");
-const inputField = document.getElementById("input");
-const inputField2 = document.getElementById("input2");
-const btnSub = document.getElementById("minusBtn")
-const btnSub2 = document.getElementById("minusBtn2")
+var btnAdd = document.getElementById("plusBtn");
+var btnAdd2 = document.getElementById("plusBtn2");
+var btnSub = document.getElementById("minusBtn")
+var btnSub2 = document.getElementById("minusBtn2")
+var inputField = document.getElementById("input");
+var inputField2 = document.getElementById("input2");
 
 addBTN(btnAdd,inputField);
 addBTN(btnAdd2,inputField2);
 subBtn(btnSub,inputField);
 subBtn(btnSub2,inputField2);
 
-// function addBTN(a,b){
-//     if(a.addEventListener("click"),function(){
-//         b.value = parseInt(b.value) + 1;
-//     }){
-//         function addValue(){
-//             b.value = parseInt(b.value) + 1;
-//             //  var products = b.value;
-//             //  return products;
-//         };
-//     }
-//     else{
-//         b.value = 0;
-//     }
-//     }
+var a = inputField.value;
+var b = parseFloat(a);
+
+var c = inputField2.value;
+var d = parseFloat(c);
+
 
 function addBTN(a,b){
     a.addEventListener("click",function(){
@@ -40,16 +32,24 @@ function subBtn(a,b){
 }
 // Plus Minus Button End
 
-const costText = document.getElementById("price").innerText;
-const cost = parseFloat(costText);
-const costText2 = document.getElementById("price2").innerText;
-const cost2 = parseFloat(costText2);
+var costText = document.getElementById("price").innerText;
+var cost = parseFloat(costText);
+var costText2 = document.getElementById("price2").innerText;
+var cost2 = parseFloat(costText2);
 
 
 
 // var a = document.getElementById("input").value;
- var qun = inputField.value;
- var quantity = parseFloat(qun);
 
- var qun2 = inputField2.value;
- var quantity2 = parseFloat(qun2);
+//  function totalPrice(a,b){
+//      var price = a* b;
+//      return price;
+//  }
+//  var p1 =  totalPrice(inputField,cost);
+//  console.log(p1);
+
+//  var qun = inputField.value;
+//  var quantity = parseFloat(qun);
+
+//  var qun2 = inputField2.value;
+//  var quantity2 = parseFloat(qun2);
